6b1598713ebdceaac2f7e70f7f60774eaf42fe42
Joel Shuart <joelshuart@gmail.com>
2019-02-28 10:41:04 -0500

    select dropdown now built with filter module

--- js/Filter.js
+++ js/Filter.js
@@ -12,10 +12,12 @@ function FiltersBar()
     
     //get possible sign languages and build filter for signs
     var signs = ["fsl_luzon", "fsl_visayas", "fsl_mindanao"]; //note: hardcoded for now - later will get from json files or database
-    var signsHTML = BuildMultiSelectFilter("SignLauageFilter", "Sign Language", signs);
-    $('#filterBar').append(signsHTML);
+    var signsHTML = BuildMultiSelectFilter("SignLauageFilter", "Sign Language", signs); //build out html for signs filter
+    $('#filterBar').append(signsHTML); //apend filter bar to have signs html
     
-    
+    var sortByFields = ["Title", "Author", "DatePublished", "LastUpdated", "Relevance"];
+    var sortByHTML = BuildSelectFilter("SortByFilter", "Sort By", sortByFields);
+    $('#filterBar').append(sortByHTML);
 }
 
 /*
@@ -47,6 +49,40 @@ function BuildMultiSelectFilter(filterID, filterName, filterOptions)
     
     //close divs and the like
     filterHTML += "</div>";
+    filterHTML += "\n";
+    filterHTML += "</div>";
+    
+    //give back final built html
+    return filterHTML;
+}
+
+/*
+Returns HTML as string for a filter field
+>filterID: tag to be used for divs id
+>filterName: text on the filter dropdown button
+>filterOptions: array of strings that define the values and text for each select option
+*/
+function BuildSelectFilter(filterID, filterName, filterOptions)
+{
+    //build base filter div
+    var filterHTML = "<div class = \"filter\" id = \"" + filterID +"\">";
+    filterHTML += "\n";
+    filterHTML += "<button>" + filterName + "</button>";
+    filterHTML += "\n";
+    filterHTML += "<select id = \"options\">";
+    filterHTML += "\n";
+    
+    //build options
+    for(var i = 0; i < filterOptions.length; i++)
+    {
+        filterHTML += "<option type = \"checkbox\" ";
+        filterHTML += "value = \"" + filterOptions[i] + "\">";
+        filterHTML += filterOptions[i];
+        filterHTML += "</option>";
+    }
+    
+    //close divs and the like
+    filterHTML += "</select>";
     filterHTML += "\n";
     filterHTML += "</div>";
     
