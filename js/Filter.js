7e87442618ca8b3c149f62213b13f8be67fbd792
Joel Shuart <joelshuart@gmail.com>
2019-02-28 17:06:49 -0500

    multi select filters now drive actual data so it can be used to filter against, upadted json to have proper language name

--- js/Filter.js
+++ js/Filter.js
@@ -1,25 +1,47 @@
 import _ from 'underscore';
 export default FiltersBar
 
+/* ----------------------- Global variables ----------------------- */
+var filters = {};
+
 /* ----------------------- Filter Building ----------------------- */
 /*
 Builds out filters bar for web pages that already have a div witht he "FIltersBar" id
 */
 function FiltersBar()
 {
-    //make filter section in html
-    //$('.FilterBar')
-    
-    //get possible sign languages and build filter for signs
+//---SIGN LANGUAGE FILTER
+    //build html for filter
+    var signID = "SignLanguageFilter";
     var signs = ["fsl_luzon", "fsl_visayas", "fsl_mindanao"]; //note: hardcoded for now - later will get from json files or database
-    var signsHTML = BuildMultiSelectFilter("SignLauageFilter", "Sign Language", signs); //build out html for signs filter
-    $('.filterBar').append(signsHTML); //apend filter bar to have signs html
-    $('#SignLauageFilter > button').on('click', function() {ToggleOptionsVisible("SignLauageFilter")});
+    var signsHTML = BuildMultiSelectFilter(signID, "Sign Language", signs); //build out html for signs filter
     
+    //update pahe html to ahve this filter
+    $('.filterBar').append(signsHTML); //apend filter bar to have signs html
+    
+    //add click events for filter functionality
+    $('#' + signID + ' > button').on('click', function() {ToggleOptionsVisible(signID)}); //toggle showing filter options
+    $('#' + signID + ' > #options > input').on('click', function(e) {UpdateMultiSelectFilter(signID, e)}); //update filter
+    
+//---WRITTEN LANGUAGE FILTER
+    //build html for filter
+    var writtenID = "WrittenLanguageFilter";
+    var written = ["English", "Tagalog"]; //note: hardcoded for now - later will get from json files or database
+    var writtenHTML = BuildMultiSelectFilter(writtenID, "Written Language", written); //build out html for signs filter
+    
+    //update page html to have this filter
+    $('.filterBar').append(writtenHTML); //apend filter bar to have signs html
+    
+    //add click events for filter functionality
+    $('#' + writtenID + ' > button').on('click', function() {ToggleOptionsVisible(writtenID)});
+    $('#' + writtenID + ' > #options > input').on('click', function(e) {UpdateMultiSelectFilter(writtenID, e)});
+    
+//---SORTING FILTER
+    var sortID = "SortByFilter";
     var sortByFields = ["Title", "Author", "DatePublished", "LastUpdated", "Relevance"];
-    var sortByHTML = BuildSelectFilter("SortByFilter", "Sort By", sortByFields);
+    var sortByHTML = BuildSelectFilter(sortID, "Sort By", sortByFields);
     $('.filterBar').append(sortByHTML);
-    $('#SortByFilter > button').on('click', function() {ToggleOptionsVisible("SortByFilter")});
+    $('#' + sortID + ' > button').on('click', function() {ToggleOptionsVisible(sortID)});
 }
 
 /*
@@ -30,6 +52,14 @@ Returns HTML as string for a filter field
 */
 function BuildMultiSelectFilter(filterID, filterName, filterOptions)
 {
+    //varibles for this filters data
+    var thisFilter = 
+    {
+        Data: [],
+        ID: filterID,
+        Name: filterName
+    }
+    
     //build base filter div
     var filterHTML = "<div class = \"filter\" id = \"" + filterID +"\">";
     filterHTML += "\n";
@@ -41,18 +71,25 @@ function BuildMultiSelectFilter(filterID, filterName, filterOptions)
     //build options
     for(var i = 0; i < filterOptions.length; i++)
     {
+        //build html
         var optionName = filterName + i.toString();
         filterHTML += "<input type = \"checkbox\" ";
         filterHTML += "name = \"" + optionName + "\"";
         filterHTML += "value = \"" + filterOptions[i] + "\">";
         filterHTML += filterOptions[i];
         filterHTML += "<br>";
+        
+        //update this filters data
+        thisFilter.Data[filterOptions[i]] = false;
     }
     
     //close divs and the like
     filterHTML += "</div>";
     filterHTML += "\n";
     filterHTML += "</div>";
+    
+    //add this fully built filter to array of filters
+    filters[thisFilter.ID] = thisFilter;
     
     //give back final built html
     return filterHTML;
@@ -115,15 +152,12 @@ function ToggleOptionsVisible(target)
 
 /* ----------------------- Filter Functionality ----------------------- */
 //Changes what vidoe results user gets based on sign
-function FilterSignLanuage()
+function UpdateMultiSelectFilter(filterData, target)
 {
-
-}
-
-//Chnages what vidoes users get based on written lang
-function FilterWrittenLanguage()
-{
-
+    //update filter data obj
+    var obj = $(target.target)[0];
+    filters[filterData].Data[obj.value] = obj.checked;
+    
 }
 
 //variable to hold current sort by type- my attempt at a javascript enum
