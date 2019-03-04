aba9f6291f3369268340c55ac5c0b3065a56e90c
CORP\stan <stan@secondavenuelearning.com>
2019-03-04 10:34:58 -0500

    started story preview module

--- js/_Main.js
+++ js/_Main.js
@@ -1,4 +1,6 @@
+import 'style/Main.css!';
 import Carousel from 'js/Carousel';
+import StoryPreview from 'js/StoryPreview';
 
 
 $(document).ready(function(){
@@ -12,4 +14,14 @@ $(document).ready(function(){
                                      'img/carousel/from_this_author/2.png',
                                      'img/carousel/from_this_author/3.png', 'img/carousel/from_this_author/3.png', 
                                      'img/carousel/from_this_author/3.png' ], 3);
+    for(var i=0; i<9; i++){
+        let sp = new StoryPreview({
+            id: i+1,
+            title: 'Aesop Fables: The Clever Donkey',
+            author: 'Massimo V.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ex nibh, euismod in arcu quis, porttitor tincidunt ipsum. Orci varius natoque penatibus et magnis dis.',
+            coverImage: 'img/carousel/from_this_author/1.png'
+        });
+        sp.appendTo('stories');
+    }
 })
