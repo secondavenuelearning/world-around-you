8c2c8fec56a7c200edffafd5dc3b9b07a605361b
Chase Slattery <slatterychase@gmail.com>
2019-03-05 14:12:30 -0500

    Fixed directional buttons on carousel to be centered

--- js/Carousel.js
+++ js/Carousel.js
@@ -9,67 +9,65 @@ var index = 0;
 
 
 var numberToShow = 0;
+
 function createPanelList(panels, showCount, startIndex, endIndex, pictures, holder) {
     for (var x = 0; x < pictures.length; x++) {
         var imageTagRepeat = document.createElement("IMG");
         imageTagRepeat.setAttribute("id", "test");
         imageTagRepeat.setAttribute("src", pictures[x]);
         imageTagRepeat.style.height = "152px";
-        
-        
-        var percentage = 100/showCount;
-        var leftStart = (180 * (percentage/100)) * x;
-        imageTagRepeat.style.left = leftStart.toString() + "px"; 
+
+
+        var percentage = 100 / showCount;
+        var leftStart = (180 * (percentage / 100)) * x;
+        imageTagRepeat.style.left = leftStart.toString() + "px";
         imageTagRepeat.style.width = percentage.toString() + "%";
-        
-        if(showCount == 1){
-          imageTagRepeat.style.width = "50%"; 
+
+        if (showCount == 1) {
+           
             imageTagRepeat.style.paddingLeft = "25%";
             imageTagRepeat.style.paddingRight = "25%";
-             imageTagRepeat.style.backgroundColor = "blue";
+            imageTagRepeat.style.backgroundColor = "#0098ba";
+        } else {
+            imageTagRepeat.style.backgroundColor = "gray";
         }
-        else{
-             imageTagRepeat.style.backgroundColor = "gray";
-        }
-        
+
         holder.appendChild(imageTagRepeat);
 
         panels[x] = imageTagRepeat;
     }
+
 }
 
 function drawPanels(panels, showCount, startIndex, endIndex, pictures, holder, direction, offset) {
-      
-        for (var x = 0; x < pictures.length; x++) {
-            if(x >= startIndex && x <=endIndex){
-                panels[x].style.display = "inline-block";
-            }
-            else{
-                panels[x].style.display = "inline-block";
-            }
-            var amountToTranslate = -100 * offset;
-            
-            if(direction == "right"){
-              
-                panels[x].style.transform = "translateX(" + amountToTranslate + "%)";
-                panels[x].style.transition = "all 2s";
-            }
-            else if(direction == "left"){
-       
-                    panels[x].style.transform = "translateX(" + amountToTranslate.toString() + "%)";
-                panels[x].style.transition = "all 2s";
-                    }
-            else{
-                        
-                    }
-            
-         
-    
 
+    for (var x = 0; x < panels.length; x++) {
+        if (x >= startIndex && x <= endIndex) {
+            panels[x].style.display = "inline-block";
+        } else {
+            panels[x].style.display = "inline-block";
+        }
+        var amountToTranslate = -100 * offset;
+
+        if (direction == "right") {
+
+            panels[x].style.transform = "translateX(" + amountToTranslate + "%)";
+            panels[x].style.transition = "all 2s";
+        } else if (direction == "left") {
+
+            panels[x].style.transform = "translateX(" + amountToTranslate.toString() + "%)";
+            panels[x].style.transition = "all 2s";
+        } else {
 
         }
 
+
+
+
+
     }
+
+}
 
 
 function Carousel(id, imageList, showing) {
@@ -111,36 +109,36 @@ function Carousel(id, imageList, showing) {
 
     parent = document.getElementById("carousel-" + tempIndex.toString());
 
-    parent.parentElement.style.backgroundColor = "blue";
+    parent.parentElement.style.backgroundColor = "#0098ba";
     parent.parentElement.style.paddingLeft = "10%";
     parent.parentElement.style.paddingRight = "10%";
     parent.parentElement.style.textAlign = "center";
-    
+
     holder = parent.querySelector("div");
 
 
     var buttonRight = parent.getElementsByClassName("right")[0];
-    
+
 
     buttonRight.onclick = function () {
-          
+
         if (endIndex >= imageList.length - 1) {
 
         } else {
             offset += showing;
             startIndex = startIndex + showing;
             endIndex = endIndex + showing;
-            drawPanels(panels, showCount, startIndex, endIndex, pictures, holder, "right",offset);
+            drawPanels(panels, showCount, startIndex, endIndex, pictures, holder, "right", offset);
         }
-        
 
-      
+
+
 
 
     };
     var buttonLeft = parent.getElementsByClassName("left")[0];
     buttonLeft.onclick = function () {
-        
+
         if (startIndex <= 0) {
 
         } else {
@@ -148,15 +146,15 @@ function Carousel(id, imageList, showing) {
             startIndex = startIndex - showing;
             endIndex = endIndex - showing;
         }
-        
-       drawPanels(panels, showCount, startIndex, endIndex, pictures, holder, "left", offset);
+
+        drawPanels(panels, showCount, startIndex, endIndex, pictures, holder, "left", offset);
 
 
     };
 
     createPanelList(panels, showCount, startIndex, endIndex, pictures, holder);
     drawPanels(panels, showCount, startIndex, endIndex, pictures, holder, null);
-   
+
 }
 
 export default Carousel
