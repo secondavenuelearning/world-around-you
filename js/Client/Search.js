deecb34bbbc61acbcbd758e05dce9fb78e0c2a8d
CORP\stan <stan@secondavenuelearning.com>
2019-03-19 17:38:48 -0400

    updates to search page

--- js/Client/Search.js
+++ js/Client/Search.js
@@ -9,27 +9,27 @@ import html from 'html/Client/Search.html!text';
 $(document).ready(function () {
     $('main').html(html);
 
-    $('#search-input').val(location.search.replace('?s=', ''));
-    $('#search-input').on('keyup', (evt) => {
-    	if(evt.key == 'Enter')
-    		RunSearch();
-    });
-    $('#submit-search').on('click', RunSearch);
-    FiltersBar('filter-bar');
-    RunSearch();
-});
+    $('#search-input').val(location.search.replace('?search=', ''));
+    var search = $('#search-input').val();
 
-function RunSearch(){
-	var search = $('#search-input').val();
-	
+
     $.ajax({
         method: 'get',
         url: `./api/search/${search}`
     }).done((stories) => {
-    	$('#stories').html('');
+        $('#search-input').on('keyup', (evt) => {
+            if(evt.key == 'Enter')
+                $('#search-wrapper').submit();
+        });
+
+        FiltersBar('filter-bar');
+
         for (var i = 0; i < stories.length; i++) {
             let sp = new StoryPreview(stories[i]);
             sp.appendTo('stories');
         }
+        
+        LanguageSelector.updateLanguageDisplay();
     });
-}
+
+});
