cd1ab97120101730932ed36412ff17681a603597
Joel Shuart <joelshuart@gmail.com>
2019-03-25 14:57:42 -0400

    added some of the images, adjusted main and header space

--- js/Client/BusGame.js
+++ js/Client/BusGame.js
@@ -3,6 +3,16 @@ import html from 'html/Client/BusGame.html!text';
 import BusGame from 'js/Client/BusGame_Game.js';
 import header from 'html/Client/BusGame_Header.html!text';
 
+//sort of an enumaerator- nothing to contain but given strict options
+var screens =
+    {
+        MainMenu: 0,
+        HowTo: 1,
+        Game: 2,
+        Win: 3
+    };
+var currentScreen = screens.Game;
+
 
 $(document).ready(function () {
     //update main section of page
