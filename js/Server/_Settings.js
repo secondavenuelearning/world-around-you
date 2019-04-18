6e8382ebe10710d0f558b0d6efaa4155266d9caa
CORP\stan <stan@secondavenuelearning.com>
2019-04-18 09:49:42 -0400

    added some encryption stuff for passwords

--- js/Server/_Settings.js
+++ js/Server/_Settings.js
@@ -1,8 +1,15 @@
 module.exports = {
+	// Server
 	port: 3000,
+
+	// Database
 	dbHost: 'localhost', 
 	dbUser: 'root', 
 	dbPassword: '',
 	dbName: 'worldaroundyou',
-	dbPoolConnectionLimit: 10
+	dbPoolConnectionLimit: 10,
+
+	// Encryption
+	algo: 'aes256',
+	key: 'ThisStringNeedsToBe32Characters.',
 }
